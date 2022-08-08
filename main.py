b9d0fcc971c239157c576f60f65bb38490258d4f
Migrok <migrok@vivaldi.net>
2022-08-08 18:30:18 +0300

    inline button

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import telebot
+from  telebot import types
 
 bot_api_token = open('bot_api_token.txt', 'r')
 bot = telebot.TeleBot(bot_api_token.read())
 bot_api_token.close()
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -11,7 +13,25 @@ def start(message):
            f'Напоминания могу присылать двумя способами:\n' \
            f'<b>- один раз в определённую дату и время\n' \
            f'- задаваемое количество раз с определённой периодичностью</b>'
+
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    menu_button = types.InlineKeyboardButton('Меню', callback_data='menu')
+    markup.add(menu_button)
+    bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(commands=['menu'])
+def menu(message):
+    mess = f'<b>Выбери действие</b>'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
-bot.polling(none_stop=True)
+@bot.callback_query_handler(func=lambda call:True)
+def callback_inline(call):
+    if call.message:
+        if call.data == 'menu':
+            bot.send_message(call.message.chat.id, '/menu')
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
